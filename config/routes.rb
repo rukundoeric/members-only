77a1b41b9f1cd2694ffde7ae75e4176b5f6c5b46
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-11-26 11:28:07 +0200

    Add User and Post models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'posts#new'
+  resources :posts, only: [:index, :new, :create]
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
